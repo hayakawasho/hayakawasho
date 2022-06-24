0ca24ef8758e9c92e55c2ddcd5fdbcca63c99b8b
hayakawasho <hayakawa@pnrm.co.jp>
2022-06-25 05:46:45 +0900

    remove remix

--- app/foundation/index.ts
+++
@@ -1,9 +0,0 @@
-export * from "lake"
-
-export * from "./util/math"
-export { Do } from "./util/fp"
-
-// export * from "./error/unknown2Err"
-
-export * from "./lib/TWEEN"
-export * as THREE from "three"
