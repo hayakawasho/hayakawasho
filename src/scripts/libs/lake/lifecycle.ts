2d97bdde686c1e71aa020a340d077cda731e7d30
hayakawasho <hayakawa@pnrm.co.jp>
2023-02-07 08:24:26 +0900

    update taxi type

--- src/scripts/libs/lake/lifecycle.ts
+++ src/scripts/libs/lake/lifecycle.ts
@@ -1,7 +1,20 @@
+import type { Transition } from '@unseenco/taxi'
 import { useUnmount } from 'lake'
 import { taxi } from '../taxi'
 
-export const useOnEnter = (callback: (payload: { from: any; to: any; trigger: any }) => void) => {
+type Payload = Parameters<Transition['enter']>[0] & {
+  from: Parameters<Transition['leave']>[0]['from']
+}
+
+export const useBeforeEnter = (callback: (payload: Omit<Payload, 'from'>) => void) => {
+  taxi.on('NAVIGATE_IN', callback)
+
+  useUnmount(() => {
+    taxi.off('NAVIGATE_IN', callback)
+  })
+}
+
+export const useOnEnter = (callback: (payload: Payload) => void) => {
   taxi.on('NAVIGATE_END', callback)
 
   useUnmount(() => {
@@ -9,7 +22,7 @@ export const useOnEnter = (callback: (payload: { from: any; to: any; trigger: an
   })
 }
 
-export const useOnLeave = (callback: (payload: { to: any; trigger: any }) => void) => {
+export const useOnLeave = (callback: (payload: Omit<Payload, 'to'>) => void) => {
   taxi.on('NAVIGATE_OUT', callback)
 
   useUnmount(() => {
