72c3ceb218be5262a0387df55a1a0bf2e6661e4b
hayakawasho <hayakawa@pnrm.co.jp>
2024-02-10 19:52:11 +0900

    chore

--- workspace/astro.config.mjs
+++ workspace/astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
-import critters from 'astro-critters';
+// import critters from 'astro-critters';
 import { glslify } from 'vite-plugin-glslify';
 import svelte from '@astrojs/svelte';
 
@@ -11,7 +11,7 @@ export default defineConfig({
   devToolbar: {
     enabled: false,
   },
-  integrations: [react(), tailwind(), critters(), svelte()],
+  integrations: [react(), tailwind(), svelte()],
   compressHTML: true,
   build: {
     inlineStylesheets: 'never',
