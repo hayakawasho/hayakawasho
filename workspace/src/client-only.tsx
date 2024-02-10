f4cc91816cfe4ea4f0b05347d90fb7e889e290ed
hayakawasho <hayakawa@pnrm.co.jp>
2024-02-10 19:56:55 +0900

    wip

--- workspace/src/client-only.tsx
+++ workspace/src/client-only.tsx
@@ -1,26 +1,20 @@
 import './entry';
-import { useEffect } from 'react';
 
 export const ClientOnly = () => {
-  useEffect(() => {
-    (async () => {
-      if (process.env.NODE_ENV === 'development') {
-        const Stats = await ((await import('https://cdn.skypack.dev/stats.js.fps?dts')) as any)
-          .default;
-        const stats = new Stats();
-        stats.showPanel(0);
-
-        document.body.appendChild(stats.dom);
-
-        const loop = () => {
-          stats.update();
-          requestAnimationFrame(loop);
-        };
-
-        loop();
-      }
-    })();
-  }, []);
-
   return <></>;
 };
+
+if (process.env.NODE_ENV === 'development') {
+  const Stats = await ((await import('https://cdn.skypack.dev/stats.js.fps?dts')) as any).default;
+  const stats = new Stats();
+  stats.showPanel(0);
+
+  document.body.appendChild(stats.dom);
+
+  const loop = () => {
+    stats.update();
+    requestAnimationFrame(loop);
+  };
+
+  loop();
+}
