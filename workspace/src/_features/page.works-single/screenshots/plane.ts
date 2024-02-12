e7ad52169931d7c3f6d442beca3b0ef5dce9004c
hayakawasho <hayakawa@pnrm.co.jp>
2024-02-12 13:40:37 +0900

    update

--- workspace/src/_features/page.works-single/screenshots/plane.ts
+++ workspace/src/_features/page.works-single/screenshots/plane.ts
@@ -47,13 +47,13 @@ export class Plane extends GlObject {
       u_bend: {
         value: {
           pc: 1.5,
-          sp: 1,
+          sp: 0.75,
         }[props.mq],
       },
       u_depth: {
         value: {
           pc: 100,
-          sp: 50,
+          sp: 40,
         }[props.mq],
       },
       u_opacity: {
