72f4545cb7a9ff76045de77936ab7ac0f3967c8e
hayakawasho <hayakawa@pnrm.co.jp>
2025-01-21 11:55:46 +0900

    wip: style

--- workspace/src/_components/model/work/navigation.tsx
+++ workspace/src/_components/model/work/navigation.tsx
@@ -2,7 +2,7 @@ import type { WorkDTO } from "../../../_models/work/dto";
 
 export function WorkNavigation({ posts, current }: { posts: WorkDTO[]; current: string }) {
   return (
-    <ul className="sp:invisible | text-[1.2rem] leading-[2] tracking-[.04em] text-right">
+    <ul className="sp:hidden | text-[1.2rem] tracking-[.04em] text-right grid gap-[.8rem]">
       {posts.map((i, index) => (
         <li key={index} className={`${current === i.id ? "font-bold" : "opacity-20"}`}>
           {i.name}
