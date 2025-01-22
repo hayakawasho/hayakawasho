752a12fa8f40d252fc7f8b3f3dbcf1cf446ffedf
hayakawasho <hayakawa@pnrm.co.jp>
2025-01-23 08:16:33 +0900

    modify

--- workspace/src/_components/model/work/navigation.tsx
+++ workspace/src/_components/model/work/navigation.tsx
@@ -2,9 +2,9 @@ import type { WorkDTO } from "../../../_models/work/dto";
 
 export function WorkNavigation({ posts, current }: { posts: WorkDTO[]; current: string }) {
   return (
-    <ul className="sp:hidden | text-[1.2rem] tracking-[.04em] text-right leading-[1.3] grid gap-[1rem]">
+    <ul className="relative z-10 text-[1.2rem] tracking-[.04em] text-right leading-[1.3] grid gap-[1rem]">
       {posts.map((i, index) => (
-        <li key={index} className={`h-[1.3em] ${current === i.id ? "font-medium" : "opacity-30 font-light"}`}>
+        <li key={index} className={`h-[1.3em] ${current === i.id ? "font-semibold" : "opacity-30 font-light"}`}>
           {i.name}
         </li>
       ))}
