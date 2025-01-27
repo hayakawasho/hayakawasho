45a8040b941fcf4cef6bb37cd3604c7d975e37d1
hayakawasho <hayakawa@pnrm.co.jp>
2025-01-27 10:54:17 +0900

    fix: safariで想定より太くなる不具合修正

--- workspace/src/_components/model/work/navigation.tsx
+++ workspace/src/_components/model/work/navigation.tsx
@@ -6,7 +6,7 @@ export function WorkNavigation({ posts, current }: { posts: WorkDTO[]; current: 
       className={`sp:text-[--color-dark] | relative z-10 text-[1.2rem] tracking-[.04em] text-right leading-[1.3] flex flex-col gap-[1.6rem]`}
     >
       {posts.map((i, index) => (
-        <li key={index} className={`h-[1.3em] my-[--leading-trim] ${current === i.id ? "font-medium" : "opacity-30"}`}>
+        <li key={index} className={`h-[1.3em] my-[--leading-trim] ${current === i.id ? "font-[599]" : "opacity-30"}`}>
           <span>{i.name}</span>
         </li>
       ))}
