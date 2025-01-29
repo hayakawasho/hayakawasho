21548c35e4b1ab30d45ce650bce696a0119d5bcd
hayakawasho <hayakawa@pnrm.co.jp>
2025-01-29 19:47:01 +0900

    wip

--- workspace/src/_components/page/work.$id/layout.tsx
+++ workspace/src/_components/page/work.$id/layout.tsx
@@ -9,7 +9,7 @@ export function Wrap({ children }: { children: React.ReactNode }) {
 export function Head({ children }: { children: React.ReactNode }) {
   return (
     <div
-      className={`sp:hidden absolute top-0 left-0 py-[4rem] px-[--grid] size-full | pc:sticky pc:right-[--gap] pc:pt-[4.8rem] pc:pb-[6rem] pc:p-[--gap] pc:h-[--100vh]`}
+      className={`sp:hidden | sticky top-0 left-0 py-[4rem] px-[--grid] size-full pc:right-[--gap] pc:pt-[4.8rem] pc:pb-[6rem] pc:p-[--gap] pc:h-[--100vh]`}
     >
       <div className="size-full relative">{children}</div>
     </div>
@@ -17,15 +17,17 @@ export function Head({ children }: { children: React.ReactNode }) {
 }
 
 export function Content({ children }: { children: React.ReactNode }) {
-  return <div className={`relative aspect-[4/5] w-[calc(var(--grid)*12)] | pc:w-full pc:h-[--100vh]`}>{children}</div>;
+  return (
+    <div className={`relative aspect-[4/5] w-[calc(var(--grid)*12)] | pc:w-full pc:min-h-[--100vh]`}>{children}</div>
+  );
 }
 
 export function Title({ children }: { children: React.ReactNode }) {
-  return <div className="absolute bottom-[2rem] left-[2rem] | pc:hidden">{children}</div>;
+  return <div className="absolute bottom-[6.4rem] right-[2rem] | pc:hidden">{children}</div>;
 }
 
 export function Trigger({ children }: { children: React.ReactNode }) {
-  return <div className="absolute z-10 bottom-[2rem] right-[2rem] | pc:hidden">{children}</div>;
+  return <div className="absolute z-10 bottom-[3.2rem] right-[2rem] | pc:hidden">{children}</div>;
 }
 
 export function Info({ children }: { children: React.ReactNode }) {
