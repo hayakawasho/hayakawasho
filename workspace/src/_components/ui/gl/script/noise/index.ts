638266845c94e703ef2f3da04384ba77be3dfca1
hayakawasho <hayakawa@pnrm.co.jp>
2025-03-10 07:15:35 +0900

    wip: glsl

--- workspace/src/_components/ui/gl/script/noise/index.ts
+++ workspace/src/_components/ui/gl/script/noise/index.ts
@@ -1,5 +1,7 @@
 import { defineComponent, useMount } from "lake";
 import { RepeatWrapping } from "three";
+// import { useMediaQuery } from "../../../../../_libs/lake/useMediaQuery";
+import { useWindowSize } from "../../../../../_libs/lake/useWindowSize";
 import {
   LinearFilter,
   Mesh,
@@ -8,11 +10,9 @@ import {
   TextureLoader,
 } from "../../../../../_libs/three";
 import { globalStore } from "../../../../../_states";
-// import { useMediaQuery } from "../../../../../_libs/lake/useMediaQuery";
-import { useWindowSize } from "../../../../../_libs/lake/useWindowSize";
 // import type { DefineComponentContext } from "../../../../../const";
-import fragmentShader from "./noise.frag";
-import vertexShader from "./noise.vert";
+// import fragmentShader from "./noise.frag";
+// import vertexShader from "./noise.vert";
 
 export default defineComponent({
   name: "RepeatNoise",
@@ -52,15 +52,38 @@ export default defineComponent({
         value: texture,
       },
       uAlpha: {
-        value: 0.06,
+        value: 1,
       },
     };
 
     const geo = new PlaneBufferGeometry(2, 2);
     const mat = new ShaderMaterial({
-      fragmentShader,
+      fragmentShader: `precision mediump float;
+
+uniform sampler2D uNoiseTexture;
+uniform float uAlpha;
+
+varying vec2 vUv;
+
+void main() {
+  vec2 uv = vUv;
+
+  vec4 noise = texture2D(uNoiseTexture, uv);
+  noise.a = uAlpha;
+
+  gl_FragColor = noise;
+}`,
       uniforms,
-      vertexShader,
+      vertexShader: `precision mediump float;
+
+varying vec2 vUv;
+uniform float uRepeat;
+
+void main() {
+  vUv = uv * uRepeat;
+  gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+}
+`,
       depthTest: false,
       transparent: true,
       alphaTest: 0.5,
